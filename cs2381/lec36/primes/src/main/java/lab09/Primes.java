35070571b0febb6533d2d603077f444d1a007768
Nat Tuck <nat@ferrus.net>
2024-11-20 12:04:33 -0500

    primes in parallel

--- cs2381/lec36/primes/src/main/java/lab09/Primes.java
+++ cs2381/lec36/primes/src/main/java/lab09/Primes.java
@@ -9,8 +9,35 @@ public class Primes {
      * How many prime numbers less than nn.
      */
     static long countPrimes(long nn) {
-        var xs = findPrimes(nn);
+        //var xs = findPrimes(nn);
+        var xs = parFindPrimes(nn);
         return xs.size();
+    }
+
+    static List<Long> parFindPrimes(long nn) {
+        long sn = Math.round(Math.sqrt(nn) + 1);
+        var ps = findPrimes(sn);
+
+        var workers = new Worker[10];
+        for (int ww = 0; ww < 10; ++ww) {
+            long i0 = ww * (nn / 10);
+            long i1 = i0 + (nn / 10);
+
+            workers[ww] = new Worker(ps, i0, i1);
+            workers[ww].start();
+        }
+
+        var ys = new ArrayList<Long>();
+        for (int ww = 0; ww < 10; ++ww) {
+            try {
+                workers[ww].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            ys.addAll(workers[ww].data);
+        }
+        return ys;
     }
 
     /**
@@ -34,11 +61,49 @@ public class Primes {
 
     // Complexity: O(n)
     static boolean isPrime(long xx, List<Long> primes) {
+        double sr = Math.sqrt(xx);
+
         for (var ii : primes) {
+            if (ii > sr) {
+                break;
+            }
             if (xx % ii == 0) {
                 return false;
             }
         }
         return true;
+    }
+}
+
+class Worker extends Thread {
+    public ArrayList<Long> data = new ArrayList<Long>();
+    List<Long> primes;
+    long i0;
+    long i1;
+
+    Worker(List<Long> primes, long i0, long i1) {
+        if (i0 < 3) {
+            data.add(2L);
+            i0 = 3;
+        }
+
+        if (i0 % 2 == 0) {
+            i0 += 1;
+        }
+
+        this.primes = primes;
+        this.i0 = i0;
+        this.i1 = i1;
+    }
+    
+    @Override
+    public void run() {
+        // search numbers from i0 to i1
+        // add any primes to data
+        for (var ii = i0; ii < i1; ii += 2) {
+            if (Primes.isPrime(ii, primes)) {
+                data.add(ii);
+            }
+        }
     }
 }
